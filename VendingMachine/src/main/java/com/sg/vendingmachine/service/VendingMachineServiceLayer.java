bff31fca2fa274e4fd524c68053d462cb8950b79
Lupita Peralta <cstuart@email.com>
2023-05-31 14:48:09 -0500

    adding service layer

---
+++ VendingMachine/src/main/java/com/sg/vendingmachine/service/VendingMachineServiceLayer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.vendingmachine.service;
+import com.sg.vendingmachine.dto.Change;
+import com.sg.vendingmachine.dto.Product;
+import java.math.BigDecimal;
+import java.util.Map;
+
+/**
+ *
+ * @author Group 2
+ */
+public interface VendingMachineServiceLayer {
+
+    public Map<String, Product> loadProductsInStock() throws VendingMachinePersistenceException,VendingMachineNoItemInventoryException;
+    public void saveProductList() throws VendingMachinePersistenceException, VendingMachineNoItemInventoryException;
+    public Product getChosenProduct(String productId) throws VendingMachinePersistenceException,VendingMachineNoItemInventoryException;
+    public void checkSufficientMoneyToBuyProduct(BigDecimal moneyIn, Product product) throws VendingMachineInsufficientFundsException;
+    public Change calculateChange(BigDecimal amount, Product product);
+    public void updateProductSale(Product product) throws VendingMachinePersistenceException, VendingMachineNoItemInventoryException;
+}
---
+++ VendingMachine/src/main/java/com/sg/vendingmachine/service/VendingMachineServiceLayerImpl.java
@@ -0,0 +1,82 @@
+package com.sg.vendingmachine.service;
+
+import com.sg.vendingmachine.dao.*;
+import com.sg.vendingmachine.dto.Change;
+import com.sg.vendingmachine.dto.Product;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Group2
+ *
+ * */
+public class VendingMachineServiceLayerImpl implements VendingMachineServiceLayer{
+    VendingMachineDao dao;
+    private VendingMachineAuditDao auditDao;
+
+    public VendingMachineServiceLayerImpl(VendingMachineDao dao, VendingMachineAuditDao auditDao){
+        this.dao=dao;
+        this.auditDao=auditDao;
+    }
+
+    @Override
+    public Map<String, Product> loadProductsInStock() throws VendingMachineNoItemInventoryException, VendingMachinePersistenceException {
+        Map<String, Product> productsInStock = new HashMap<>();
+        //lambda function to filter items that are in stock and items that are not
+        for (Product p : dao.loadProductsFromFile().values()){
+            if (p.getItemsInStock() < 1){
+                auditDao.writeAuditEntry("Product id: " + p.getProductId());
+            } else{
+                productsInStock.put(p.getProductId(), p);
+            }
+        }
+        return productsInStock;
+    }
+
+    @Override
+    public void saveProductList() throws VendingMachinePersistenceException {
+        dao.writeProductToFile();
+        auditDao.writeAuditEntry("Product list saved to file");
+    }
+
+    @Override
+     public Product getChosenProduct(String productId) throws VendingMachineNoItemInventoryException {
+        validateProductInStock(productId);
+        return dao.getProduct(productId);
+    }
+
+    @Override
+    public void checkSufficientMoneyToBuyProduct(BigDecimal inputAmount, Product product) throws VendingMachineInsufficientFundsException {
+        if(inputAmount.compareTo(product.getPrice())<0){
+            throw new VendingMachineInsufficientFundsException("Error: Insufficient funds to purchase: "+ product.getProductName());
+        }
+    }
+
+    @Override
+    public Change calculateChange(BigDecimal amount, Product product) {
+        BigDecimal change = amount.subtract(product.getPrice()).multiply(new BigDecimal("100"));
+        return new Change(change);
+    }
+
+    @Override
+    public void updateProductSale(Product product) throws VendingMachinePersistenceException, VendingMachineNoItemInventoryException {
+        if (product.getItemsInStock()>0){
+            product.setItemsInStock(product.getItemsInStock()-1);
+        }else{
+            throw new VendingMachineNoItemInventoryException("Error: Selected product is not in stock");
+        }
+        dao.updateProduct(product.getProductId(), product);
+        auditDao.writeAuditEntry("Product ID: "+ product.getProductId()+" is updated");
+    }
+
+    private void validateProductInStock(String productId) throws VendingMachineNoItemInventoryException {
+        List<String>ids=dao.getAllProductsIds();
+        Product product = dao.getProduct(productId);
+        if(!ids.contains(productId)||product.getItemsInStock()<1){
+            throw new VendingMachineNoItemInventoryException("Error: Selected product is not in stock");
+        }
+    }
+}
