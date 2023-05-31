19a6799f96ab9199f6ed078426332f14c2ee87bd
Rania Ouassif <rania.ouassif@mail.mcgill.ca>
2023-05-31 13:15:31 -0400

    VendingMachineAudit

--- VendingMachine/src/main/java/com/sg/vendingmachine/dao/VendingMachineAuditDao.java
+++ VendingMachine/src/main/java/com/sg/vendingmachine/dao/VendingMachineAuditDao.java
@@ -1,4 +1,6 @@
 package com.sg.vendingmachine.dao;
 
 public interface VendingMachineAuditDao {
+    //TODO fix exception
+    public void writeAuditEntry(String entry) throws Exception;
 }
--- VendingMachine/src/main/java/com/sg/vendingmachine/dao/VendingMachineAuditDaoFileImpl.java
+++ VendingMachine/src/main/java/com/sg/vendingmachine/dao/VendingMachineAuditDaoFileImpl.java
@@ -1,4 +1,26 @@
 package com.sg.vendingmachine.dao;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
 public class VendingMachineAuditDaoFileImpl  implements VendingMachineAuditDao {
+
+    public static final String AUDIT_FILE = "audit.txt";
+    @Override
+    public void writeAuditEntry(String entry) throws Exception {
+        PrintWriter out;
+
+        try {
+            out = new PrintWriter(new FileWriter(AUDIT_FILE), true);
+        } catch (IOException e) {
+            throw new Exception("Could not load data");
+        }
+
+        LocalDateTime timeStamp = LocalDateTime.now();
+        out.println(timeStamp.toString() + " : " + entry);
+        out.flush();
+    }
 }
