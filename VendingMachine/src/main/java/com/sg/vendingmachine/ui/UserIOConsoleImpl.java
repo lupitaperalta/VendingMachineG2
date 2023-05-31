3bf24d5b5d0a0e5e21c5db9897949b261f4f4c61
Rania Ouassif <rania.ouassif@mail.mcgill.ca>
2023-05-31 10:39:52 -0400

    UserIO & UserIOConsoleImpl files added

---
+++ VendingMachine/src/main/java/com/sg/vendingmachine/ui/UserIOConsoleImpl.java
@@ -0,0 +1,53 @@
+package com.sg.vendingmachine.ui;
+
+public class UserIOConsoleImpl implements UserIO {
+    @Override
+    public void print(String msg) {
+        
+    }
+
+    @Override
+    public double readDouble(String prompt) {
+        return 0;
+    }
+
+    @Override
+    public double readDouble(String prompt, double min, double max) {
+        return 0;
+    }
+
+    @Override
+    public float readFloat(String prompt) {
+        return 0;
+    }
+
+    @Override
+    public float readFloat(String prompt, float min, float max) {
+        return 0;
+    }
+
+    @Override
+    public int readInt(String prompt) {
+        return 0;
+    }
+
+    @Override
+    public int readInt(String prompt, int min, int max) {
+        return 0;
+    }
+
+    @Override
+    public long readLong(String prompt) {
+        return 0;
+    }
+
+    @Override
+    public long readLong(String prompt, long min, long max) {
+        return 0;
+    }
+
+    @Override
+    public String readString(String prompt) {
+        return null;
+    }
+}
