9bf53f2419288b1382e7e07cf495e96b7f377369
Lupita Peralta <cstuart@email.com>
2023-05-31 10:30:32 -0500

    Change modified

--- VendingMachine/src/main/java/com/sg/vendingmachine/dto/Change.java
+++ VendingMachine/src/main/java/com/sg/vendingmachine/dto/Change.java
@@ -3,11 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.sg.vendingmachine.dto;
+import java.math.BigDecimal;
 
 /**
  *
  * @author Group 2
  */
 public class Change {
+    private int quarters;
+    private int dimes;
+    private int nickels;
+    private int pennies;
+    public Change(BigDecimal amount){
+        this.quarters=amount.divide(new BigDecimal("25")).intValue();
+        amount=amount.remainder(new BigDecimal("25"));
+        this.dimes=amount.divide(new BigDecimal("10")).intValue();
+        amount=amount.remainder(new BigDecimal("10"));
+        this.nickels=amount.divide(new BigDecimal("5")).intValue();
+        this.pennies=amount.remainder(new BigDecimal("5")).intValue();
+    }
+    public int getQuarters(){
+        return quarters;
+    }
+    public int getDimes(){
+        return dimes;
+    }
+    public int getNickels(){
+        return nickels;
+    }
+    public int getPennies(){
+        return pennies;
+    }
     
 }
